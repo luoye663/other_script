6671fe3f314e517705fca03a1873e7d337b21bdc
Zheng Jie <201507802@qq.com>
2022-04-06 22:13:27 +0800

    update

--- bt_7.7_happy/bt_happy.sh
+++ bt_7.7_happy/bt_happy.sh
@@ -32,13 +32,17 @@ if [ -f ${plugin_file} ];then
     cd /www/server/panel/data
     wget https://proxy.zyun.vip/https://raw.githubusercontent.com/elunez/other_script/master/bt_7.7_happy/plugin.json
     chattr +i /www/server/panel/data/plugin.json
+    # 去除软件商城广告
+    chattr -i /www/server/panel/BTPanel/templates/default/soft.html
     rm /www/server/panel/BTPanel/templates/default/soft.html
     cd /www/server/panel/BTPanel/templates/default
     wget https://proxy.zyun.vip/https://raw.githubusercontent.com/elunez/other_script/master/bt_7.7_happy/soft.html
+    chattr +i /www/server/panel/BTPanel/templates/default/soft.html
 else
     cd /www/server/panel/data
     wget https://proxy.zyun.vip/https://raw.githubusercontent.com/elunez/other_script/master/bt_7.7_happy/plugin.json
     chattr +i /www/server/panel/data/plugin.json
+    # 去除软件商城广告
     cd /www/server/panel/BTPanel/templates/default
     wget https://proxy.zyun.vip/https://raw.githubusercontent.com/elunez/other_script/master/bt_7.7_happy/soft.html
 fi
