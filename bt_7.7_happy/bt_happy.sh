342036f68006b971220a48ea605be3d85e2efd79
Zheng Jie <201507802@qq.com>
2022-05-13 13:18:49 +0800

    update

--- bt_7.7_happy/bt_happy.sh
+++ bt_7.7_happy/bt_happy.sh
@@ -1,13 +1,21 @@
-#!/bin/bash
-LANG=en_US.UTF-8
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 
 echo "
 +----------------------------------------------------------------------
-| Bt-WebPanel-Happy FOR CentOS
-+----------------------------------------------------------------------
-| 本脚本用于宝塔面板7.7版本的一键开心，因为脚本造成的问题请自行负责！
+| 用于宝塔面板7.7版本的一键优化，因为脚本造成的问题请自行负责！
 +----------------------------------------------------------------------
 "
+
+if [ $(whoami) != "root" ];then
+    echo "请使用root权限执行命令！"
+    exit 1;
+fi
+if [ ! -d /www/server/panel ] || [ ! -f /etc/init.d/bt ];then
+    echo "未安装宝塔面板"
+    exit 1
+fi 
+
 while [ "$go" != 'y' ] && [ "$go" != 'n' ]
 do
     read -p "请确认你已经安装的版本是7.7，请确保仅用于学习使用！(y/n): " go;
@@ -17,39 +25,75 @@ if [ "$go" == 'n' ];then
     exit;
 fi
 
-#修改强制登录开始
-sed -i "s|if (bind_user == 'True') {|if (bind_user == 'REMOVED') {|g" /www/server/panel/BTPanel/static/js/index.js
-rm -rf /www/server/panel/data/bind.pl
-#修改强制登录结束
-echo -e "修改强制登陆中..."
-sleep 2
-echo -e "插件商城开心开始..."
-#判断plugin.json文件是否存在,存在删除之后再下载,不存在直接下载
-plugin_file="/www/server/panel/data/plugin.json"
-if [ -f ${plugin_file} ];then
-    chattr -i /www/server/panel/data/plugin.json
-    rm /www/server/panel/data/plugin.json
-    cd /www/server/panel/data
-    wget https://proxy.zyun.vip/https://raw.githubusercontent.com/elunez/other_script/master/bt_7.7_happy/plugin.json
-    chattr +i /www/server/panel/data/plugin.json
-    # 去除软件商城广告
+if [ ! -f /www/server/panel/data/userInfo.json ]; then
+    echo "{\"uid\":1000,\"username\":\"admin\",\"serverid\":1}" > /www/server/panel/data/userInfo.json
+fi
+echo "已去除宝塔面板强制绑定账号."
+
+Layout_file="/www/server/panel/BTPanel/templates/default/layout.html";
+JS_file="/www/server/panel/BTPanel/static/bt.js";
+if [ `grep -c "<script src=\"/static/bt.js\"></script>" $Layout_file` -eq '0' ];then
+    sed -i '/{% block scripts %} {% endblock %}/a <script src="/static/bt.js"></script>' $Layout_file;
+fi;
+wget -q http://f.cccyun.cc/bt/bt.js -O $JS_file;
+echo "已去除各种计算题与延时等待."
+
+sed -i "/htaccess = self.sitePath+'\/.htaccess'/, /public.ExecShell('chown -R www:www ' + htaccess)/d" /www/server/panel/class/panelSite.py
+sed -i "/index = self.sitePath+'\/index.html'/, /public.ExecShell('chown -R www:www ' + index)/d" /www/server/panel/class/panelSite.py
+sed -i "/doc404 = self.sitePath+'\/404.html'/, /public.ExecShell('chown -R www:www ' + doc404)/d" /www/server/panel/class/panelSite.py
+echo "已去除创建网站自动创建的垃圾文件."
+
+sed -i "s/root \/www\/server\/nginx\/html/return 400/" /www/server/panel/class/panelSite.py
+if [ -f /www/server/panel/vhost/nginx/0.default.conf ]; then
+    sed -i "s/root \/www\/server\/nginx\/html/return 400/" /www/server/panel/vhost/nginx/0.default.conf
+fi
+echo "已关闭未绑定域名提示页面."
+
+sed -i "s/return render_template('autherr.html')/return abort(404)/" /www/server/panel/BTPanel/__init__.py
+echo "已关闭安全入口登录提示页面."
+
+sed -i "/p = threading.Thread(target=check_files_panel)/, /p.start()/d" /www/server/panel/task.py
+sed -i "/p = threading.Thread(target=check_panel_msg)/, /p.start()/d" /www/server/panel/task.py
+echo "已去除消息推送与文件校验."
+
+sed -i "/^logs_analysis()/d" /www/server/panel/script/site_task.py
+sed -i "s/run_thread(cloud_check_domain,(domain,))/return/" /www/server/panel/class/public.py
+echo "已去除面板日志与绑定域名上报."
+
+if [ ! -f /www/server/panel/data/not_recommend.pl ]; then
+    echo "True" > /www/server/panel/data/not_recommend.pl
+fi
+if [ ! -f /www/server/panel/data/not_workorder.pl ]; then
+    echo "True" > /www/server/panel/data/not_workorder.pl
+fi
+echo "已关闭活动推荐与在线客服."
+
+if [ -f /www/server/panel/BTPanel/templates/default/soft.html ];then
+    chattr -i /www/server/panel/BTPanel/templates/default/soft.html
     rm -rf /www/server/panel/BTPanel/templates/default/soft.html
     cd /www/server/panel/BTPanel/templates/default
     wget https://proxy.zyun.vip/https://raw.githubusercontent.com/elunez/other_script/master/bt_7.7_happy/soft.html
     chattr +i /www/server/panel/BTPanel/templates/default/soft.html
 else
-    cd /www/server/panel/data
-    wget https://proxy.zyun.vip/https://raw.githubusercontent.com/elunez/other_script/master/bt_7.7_happy/plugin.json
-    chattr +i /www/server/panel/data/plugin.json
-    # 去除软件商城广告
     cd /www/server/panel/BTPanel/templates/default
     wget https://proxy.zyun.vip/https://raw.githubusercontent.com/elunez/other_script/master/bt_7.7_happy/soft.html
 fi
-sleep 3
-echo -e "去除升级红点开始..."
-#判断index.html文件是否存在,存在删除之后再下载,不存在直接下载
-index_file="/www/server/panel/BTPanel/templates/default/index.html"
-if [ -f ${index_file} ];then
+echo "已去除商城界面广告."
+
+if [ -f /www/server/panel/data/plugin.json ];then
+    chattr -i /www/server/panel/data/plugin.json
+    rm /www/server/panel/data/plugin.json
+    cd /www/server/panel/data
+    wget https://proxy.zyun.vip/https://raw.githubusercontent.com/elunez/other_script/master/bt_7.7_happy/plugin.json
+    chattr +i /www/server/panel/data/plugin.json
+else
+    cd /www/server/panel/data
+    wget https://proxy.zyun.vip/https://raw.githubusercontent.com/elunez/other_script/master/bt_7.7_happy/plugin.json
+    chattr +i /www/server/panel/data/plugin.json
+fi
+echo "已去除插件安装限制."
+
+if [ -f /www/server/panel/BTPanel/templates/default/index.html ];then
     rm /www/server/panel/BTPanel/templates/default/index.html
     cd /www/server/panel/BTPanel/templates/default
     wget https://proxy.zyun.vip/https://raw.githubusercontent.com/elunez/other_script/master/bt_7.7_happy/index.html
@@ -57,11 +101,9 @@ else
     cd /www/server/panel/BTPanel/templates/default
     wget https://proxy.zyun.vip/https://raw.githubusercontent.com/elunez/other_script/master/bt_7.7_happy/index.html
 fi
-sleep 3
-echo -e "文件防止修改开始..."
-#判断repair.json文件是否存在,存在删除之后再下载,不存在直接下载
-repair_file="/www/server/panel/data/repair.json"
-if [ -f ${repair_file} ];then
+echo -e "已去除首页升级红点."
+
+if [ -f /www/server/panel/data/repair.json ];then
     chattr -i /www/server/panel/data/repair.json
     rm /www/server/panel/data/repair.json
     cd /www/server/panel/data
@@ -72,11 +114,12 @@ else
     wget https://proxy.zyun.vip/https://raw.githubusercontent.com/elunez/other_script/master/bt_7.7_happy/repair.json
     chattr +i /www/server/panel/data/repair.json
 fi
-sleep 3
-     /etc/init.d/bt restart
-sleep 2 
-echo -e "修改强制登陆结束."
-echo -e "插件商城开心结束."
-echo -e "去除升级红点结束."
-echo -e "文件防止修改结束."
-echo -e "宝塔面板开心结束！"
+echo -e "文件防止修改完成."
+
+/etc/init.d/bt restart
+
+echo -e "=================================================================="
+echo -e "\033[32m宝塔面板优化执行完毕\033[0m"
+echo -e "=================================================================="
+echo  "如需还原之前的样子，请在面板首页点击“修复”"
+echo -e "=================================================================="
